e65c68080ae0cf2685d083d35571f079341fda18
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-09-01 16:54:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Contains;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var numbers = new List<int>()
+            { 107, 92, 3, 46, 5096, 666, 85 };
+
+        Console.WriteLine(numbers.Contains(6));
+        Console.WriteLine(numbers.Contains(3));
+    }
+}
+
