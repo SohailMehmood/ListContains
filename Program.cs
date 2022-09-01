aa0f283a184fbb1e1fea09f63f3bbf96d2a2cbc4
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-09-01 16:55:35 +0100

    added null check

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
         var numbers = new List<int>()
             { 107, 92, 3, 46, 5096, 666, 85 };
 
